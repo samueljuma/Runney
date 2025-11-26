866eec3b9107367027a6349283afe2fff2309795
samueljuma <samueljuma1024@gmail.com>
2025-11-26 13:30:40 +0300

    add Icons to designstystem

--- app/src/main/java/com/phillqins/runney/MainActivity.kt
+++ app/src/main/java/com/phillqins/runney/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.phillqins.core.presentation.designsystem.RunneyTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +16,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             RunneyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RunneyTheme {
-        Greeting("Android")
     }
 }
