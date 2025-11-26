1d638234e40e2b4655631b8cdab61431f5c7decf
samueljuma <samueljuma1024@gmail.com>
2025-11-26 12:49:40 +0300

    set up theme

--- app/src/main/java/com/phillqins/runney/MainActivity.kt
+++ app/src/main/java/com/phillqins/runney/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.phillqins.runney.ui.theme.RunneyTheme
+import com.phillqins.core.presentation.designsystem.RunneyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ core/presentation/designsystem/src/main/java/com/phillqins/core/presentation/designsystem/Theme.kt
@@ -0,0 +1,48 @@
+package com.phillqins.core.presentation.designsystem
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+
+val DarkColorScheme = darkColorScheme(
+    primary = RunneyGreen,
+    background = RunneyBlack,
+    surface = RunneyDarkGray,
+    secondary = RunneyWhite,
+    tertiary = RunneyWhite,
+    primaryContainer = RunneyGreen30,
+    onPrimary = RunneyBlack,
+    onBackground = RunneyWhite,
+    onSurface = RunneyWhite,
+    onSurfaceVariant = RunneyGray,
+    onTertiary = RunneyBlack
+)
+
+@Composable
+fun RunneyTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = DarkColorScheme
+    val view = LocalView.current
+    if(!view.isInEditMode){
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+
+}
