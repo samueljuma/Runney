8574d7110865aeef8a672f30a73c005ac360f524
samueljuma <samueljuma1024@gmail.com>
2025-11-27 23:30:26 +0300

    implement user-data validator

--- auth/domain/src/main/java/com/phillqins/auth/domain/UserDataValidator.kt
+++ auth/domain/src/main/java/com/phillqins/auth/domain/UserDataValidator.kt
@@ -1,6 +1,26 @@
 package com.phillqins.auth.domain
 
-class UserDataValidator {
+class UserDataValidator(
+    private val patternValidator: PatternValidator
+) {
+    fun isValidEmail(email: String): Boolean {
+        return patternValidator.matches(email.trim())
+    }
+
+    fun validatePassword(password: String): PasswordValidationState{
+        val hasMinLength = password.length >= MIN_PASSWORD_LENGTH
+        val hasNumber = password.any { it.isDigit() }
+        val hasUpperCaseCharacter = password.any { it.isUpperCase() }
+        val hasLowerCaseCharacter = password.any { it.isLowerCase() }
+
+        return PasswordValidationState(
+            hasMinLength = hasMinLength,
+            hasNumber = hasNumber,
+            hasUpperCaseCharacter = hasUpperCaseCharacter,
+            hasLowerCaseCharacter = hasLowerCaseCharacter
+        )
+
+    }
     companion object {
         const val MIN_PASSWORD_LENGTH = 9
     }
--- auth/presentation/src/main/java/com/phillqins/auth/presentation/register/RegisterViewModel.kt
+++ auth/presentation/src/main/java/com/phillqins/auth/presentation/register/RegisterViewModel.kt
@@ -3,14 +3,40 @@ package com.phillqins.auth.presentation.register
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.phillqins.auth.domain.UserDataValidator
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-class RegisterViewModel: ViewModel(){
+class RegisterViewModel(
+    private val userDataValidator: UserDataValidator
+): ViewModel(){
     var state by mutableStateOf(RegisterState())
         private set
+
+    init {
+        // Observe email changes
+        snapshotFlow { state.email.text }
+            .onEach { email ->
+                state = state.copy(
+                    isEmailValid = userDataValidator.isValidEmail(email.toString())
+                )
+            }
+            .launchIn(viewModelScope)
+
+        // Observe password changes
+        snapshotFlow { state.password.text }
+            .onEach { password ->
+                state = state.copy(
+                    passwordValidationState = userDataValidator.validatePassword(password.toString())
+                )
+            }
+            .launchIn(viewModelScope)
+    }
 
     fun onAction(action: RegisterAction){
 
     }
-
 }
